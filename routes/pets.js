a2c93c14fab5822bc858e01719985aca49093f64
sebarck <45472958+sebarck@users.noreply.github.com>
2018-12-11 13:17:05 -0300

    Se arregla path del GET

--- routes/pets.js
+++ routes/pets.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var response = "";
 
 //GET de mascotas asociadas a un ID de usuario
-router.get('/pets', function(req, res, next) {
+router.get('/', function(req, res, next) {
     //Aca deberia ir la conexion a la base para recuperar los usuarios
     response = '{"mascotas":[{"id":1,"nombre":"Pachorra","descripcion":"Es una perrita salchicha muy inquieta y juguetona"},{"id":2,"nombre":"Coki","descripcion":"Es una perrita coker negra y gordita"}]}';
     res.send(response);
