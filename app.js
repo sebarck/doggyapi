e998e05830fc3a8466a8f07aef50a520c375723e
sebarck <45472958+sebarck@users.noreply.github.com>
2018-12-11 13:03:24 -0300

    Arreglar require app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var petsRouter = requiere('./routes/pets');
+var petsRouter = require('./routes/pets');
 
 var app = express();
 
