3ca25d9c9b8b201913858dc692ad044d4d32508a
sebarck <45472958+sebarck@users.noreply.github.com>
2018-12-11 13:10:46 -0300

    Errores en pets.js y modificacion de response

--- routes/pets.js
+++ routes/pets.js
@@ -1,11 +1,12 @@
 var express = require('express');
-var router = express.Router;
+var router = express.Router();
+var response = "";
 
 //GET de mascotas asociadas a un ID de usuario
 router.get('/pets', function(req, res, next) {
     //Aca deberia ir la conexion a la base para recuperar los usuarios
-    Response = '{"mascotas":{"mascota":{"id":1,"nombre":"Pachorra","descripcion":"Es una perrita salchicha muy inquieta y juguetona"}}}';
-    res.send(Response);
-})
+    response = '{"mascotas":[{"id":1,"nombre":"Pachorra","descripcion":"Es una perrita salchicha muy inquieta y juguetona"},{"id":2,"nombre":"Coki","descripcion":"Es una perrita coker negra y gordita"}]}';
+    res.send(response);
+});
 
 module.exports = router;
