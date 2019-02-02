6ca72c393d39018b0e0fe48c4b9d7035b30caea2
Seba Monti PC <sebasmonti206@gmail.com>
2019-02-01 22:55:37 -0300

    Conexion a la base productiva

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql');
  var connection=mysql.createPool({
 
-host:'localhost',
- user:'root',
- password:'Napoli.01',
- database:'db_doggy_chapitas',
+host:'mysql.decseguros.com.ar',
+ user:'db_doggychapitas',
+ password:'TSr4D8GzxLSmOCBUrw17',
+ database:'db_doggy_test',
  port:3306
 
 });
